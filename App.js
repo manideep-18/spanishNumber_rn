f16344db90f801ae6a48665c9c885486ed6ced74
manideep-18 <r8manideep@gmail.com>
2020-12-07 14:31:26 +0530

    styles added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Text,View,ScrollView,StyleSheet,Image} from 'react-native'
+import {Text,View,ScrollView,StyleSheet,Image,TouchableOpacity} from 'react-native'
 import Sound from 'react-native-sound'
 
 const soundsList=[
@@ -16,11 +16,57 @@ const soundsList=[
 ]
 
 const App=()=>{
+
+ const handleButtonSound=()=>{
+
+  }
+
   return(
-    <ScrollView>
-      <Image source={require('./assets/logo.png')}/>
+    <ScrollView style={styles.container}>
+      <Image style={styles.logo} source={require('./assets/logo.png')}/>
+      <View style={styles.gridContainer}>
+        {soundsList.map((eachSound)=>(
+          <TouchableOpacity key={eachSound} onPress={handleButtonSound} style={styles.box}>
+            <Text style={styles.text}>{eachSound}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
     </ScrollView>
   )
 }
 
 export default App
+
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"#1b262c"
+  },
+  logo:{
+    alignSelf:"center",
+    marginTop:20
+  },
+  gridContainer:{
+    flex:1,
+    margin:5,
+    flexDirection:"row",
+    flexWrap:'wrap',
+    alignItems:"flex-start",
+    justifyContent:"space-around"
+  },
+  box:{
+    height:110,
+    alignItems:"center",
+    justifyContent:"center",
+    backgroundColor:'#0f4c75',
+    width:"46%",
+    marginVertical:6,
+    borderRadius:5,
+    shadowColor:"#393e46",
+    elevation:5
+  },
+  text:{
+    fontSize:50,
+    color:"#ff4301"
+  }
+})
